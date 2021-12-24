651e5b59aabbe167b0f7ed44dc540874c46f56eb
FarahGamal <farah.ali02@eng-st.cu.edu.eg>
2021-12-24 13:41:42 +0200

    mariam's part

--- database.py
+++ database.py
@@ -16,11 +16,16 @@ mycursor = mydb.cursor()
 #? create equipment table
 #mycursor.execute("CREATE TABLE Equipment (biocode VARCHAR(255) NOT NULL PRIMARY KEY, serial_number VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, r_check_up VARCHAR(255) NOT NULL, fk_r_no VARCHAR(255), FOREIGN KEY (fk_r_no) REFERENCES rooms (r_number))")
 #? create Surgeries
-#! fadal l foreign key pssn
+#! fadal l foreign key pssn mn table l patient
 #mycursor.execute("CREATE TABLE Surgeries (surgery_number VARCHAR(255) NOT NULL PRIMARY KEY, type VARCHAR(255) NOT NULL, start_time VARCHAR(255) NOT NULL, end_time VARCHAR(255) NOT NULL, r_no VARCHAR(255) NOT NULL, FOREIGN KEY (r_no) REFERENCES rooms (r_number))")
-
+#? create Managerial employees table
+#mycursor.execute("CREATE TABLE Managerial_employees (SSN VARCHAR(255) NOT NULL UNIQUE,fname VARCHAR(250) NOT NULL,minit VARCHAR(250) NOT NULL,lname VARCHAR(250) NOT NULL,Phone_number VARCHAR(255) NOT NULL,Email VARCHAR(250),Gender VARCHAR(250) NOT NULL,Salary VARCHAR(255) NOT NULL,Position VARCHAR(250) NOT NULL,Qualifications VARCHAR(250) NOT NULL,Address VARCHAR(250),ESSSN VARCHAR(255) NOT NULL,PRIMARY KEY (SSN),FOREIGN KEY (ESSSN) REFERENCES Managerial_employees(SSN))")
+#? create Technicians table
+#mycursor.execute("CREATE TABLE Technicians (SSN VARCHAR(255) NOT NULL UNIQUE,fname VARCHAR(250) NOT NULL,minit VARCHAR(250) NOT NULL,lname VARCHAR(250) NOT NULL,Phone_number int NOT NULL,Email VARCHAR(250),Gender VARCHAR(250) NOT NULL,Salary VARCHAR(255) NOT NULL,Position VARCHAR(250) NOT NULL,Qualifications VARCHAR(250) NOT NULL,Address VARCHAR(250),TSSSN VARCHAR(255) NOT NULL,ESSN VARCHAR(255) NOT NULL,PRIMARY KEY (SSN),FOREIGN KEY (TSSSN) REFERENCES Technicians(SSN),FOREIGN KEY (ESSN) REFERENCES Managerial_employees(SSN))")
+#? works on table
+#mycursor.execute("CREATE TABLE Works_on (MSSN VARCHAR(255) NOT NULL,Sno VARCHAR(255) NOT NULL,FOREIGN KEY (MSSN) REFERENCES Managerial_employees (SSN), FOREIGN KEY (Sno) REFERENCES Surgeries (surgery_number))")
 #!!!! FADAL L REPAIR TABLE WAITTING FOR TECHNICIANS TABLE
-
+#mycursor.execute("CREATE TABLE Repair (tssn VARCHAR(255) NOT NULL, biocode VARCHAR(255) NOT NULL, FOREIGN KEY (tssn) REFERENCES Technicians (SSN), FOREIGN KEY (biocode) REFERENCES Equipment (biocode) )")
 #? insert room numbes in room table
 '''
 sql=(" INSERT INTO Rooms (r_number) VALUES (%s) ")
@@ -33,3 +38,4 @@ val={
 mycursor.executemany(sql,val)
 mydb.commit()
 '''
+
--- server.py
+++ server.py
@@ -103,5 +103,107 @@ def viewroom():
       return render_template('view_room.html',data=data)
       
 
+#! Managerial Employees add & view
+
+@app.route('/add_ManagerialEmployees',methods = ['POST', 'GET'])
+def addManagerialEmployees():
+    if request.method == 'POST': ##check if there is post data
+     ssn = request.form['SSN']
+     fname = request.form['fname']
+     minit = request.form[' minit']
+     lname = request.form['lname']
+     Phone_number = request.form['Phone_number']
+     Email = request.form['Email']
+     Gender = request.form['Gender']
+     Salary = request.form['Salary']
+     Position = request.form['Position']
+     Qualifications = request.form['Qualifications']
+     Address = request.form['Address']
+     esssn = request.form['ESSSN']
+     #print(SSN,fname, minit,lname,Phone_number,Email, Gender,Salary,Position,Qualifications,Address,ESSSN)
+     sql = "INSERT INTO Managerial_employees (SSN,fname,minit,lname,Phone_number,Email,Gender,Salary,Position,Qualifications,Address,ESSSN) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+     val = (ssn,fname,minit,lname,Phone_number,Email,Gender,Salary,Position,Qualifications,Address,esssn)
+     mycursor.execute(sql, val)
+     mydb.commit()
+     return render_template('index.html')
+    else: 
+      return render_template ("add_ManagerialEmployees.html")
+
+@app.route('/view_ManagerialEmployees',methods = ['POST', 'GET'])
+def viewManagerialEmployees():
+     if request.method == 'POST':
+      return render_template('index.html')
+     else:
+      mycursor.execute("SELECT * FROM Managerial_employees")
+      row_headers=[x[0] for x in mycursor.description] #this will extract row headers
+      myresult = mycursor.fetchall()
+      data={
+         'message':"data retrieved",
+         'rec':myresult,
+         'header':row_headers
+        }
+      return render_template('view_ManagerialEmployees.html',data=data)
+
+
+#! Technicians add & view
+
+@app.route('/add_Technicians')
+def addTechnicians():
+    if request.method == 'POST': ##check if there is post data
+     SSN = request.form['SSN']
+     fname = request.form['fname']
+     minit = request.form[' minit']
+     lname = request.form['lname']
+     Phone_number = request.form['Phone_number']
+     Email = request.form['Email']
+     Gender = request.form['Gender']
+     Salary = request.form['Salary']
+     Position = request.form['Position']
+     Qualifications = request.form['Qualifications']
+     Address = request.form['Address']
+     TSSSN = request.form['TSSSN']
+     ESSN = request.form['ESSN']
+     #print(SSN,fname, minit,lname,Phone_number,Email, Gender,Salary,Position,Qualifications,Address,TSSSN,ESSN)
+     sql = "INSERT INTO Technicians (SSN,fname,minit,lname,Phone_number,Email,Gender,Salary,Position,Qualifications,Address,TSSSN,ESSN) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+     val = (SSN,fname,minit,lname,Phone_number,Email,Gender,Salary,Position,Qualifications,Address,TSSSN,ESSN)
+     mycursor.execute(sql, val)
+     mydb.commit()
+     return render_template('index.html')
+    else: 
+      return render_template ("add_Technicians.html")
+
+@app.route('/view_Technicians',methods = ['POST', 'GET'])
+def viewTechnicians():
+    if request.method == 'POST':
+      return render_template('index.html')
+    else:
+      mycursor.execute("SELECT * FROM Technicians")
+      row_headers=[x[0] for x in mycursor.description] #this will extract row headers
+      myresult = mycursor.fetchall()
+      data={
+         'message':"data retrieved",
+         'rec':myresult,
+         'header':row_headers
+        }
+      return render_template('view_Technicians.html',data=data)
+
+#! Works on view
+
+@app.route('/view_WorksOn',methods = ['POST', 'GET'])
+def viewWorksOn():
+    if request.method == 'POST':
+      return render_template('index.html')
+    else:
+      mycursor.execute("SELECT * FROM Works_on")
+      row_headers=[x[0] for x in mycursor.description] #this will extract row headers
+      myresult = mycursor.fetchall()
+      data={
+         'message':"data retrieved",
+         'rec':myresult,
+         'header':row_headers
+        }
+      return render_template('view_WorksOn.html',data=data)
+
+
 if __name__ == '__main__':
    app.run()
